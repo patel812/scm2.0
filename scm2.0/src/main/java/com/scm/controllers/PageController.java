5c6f76368ae316bb74b25c4a0d45a6c3961a5dd6
Abhishek Patel <abhishekpatel1238@gmail.com>
2024-05-28 16:38:25 +0530

    4. Show data dynamic from Key & Value for bakend to frontend

--- scm2.0/src/main/java/com/scm/controllers/PageController.java
+++ scm2.0/src/main/java/com/scm/controllers/PageController.java
@@ -1,14 +1,21 @@
 package com.scm.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
 
     @RequestMapping("/home")
-    public String home() {
+    public String home(Model model) {
         System.out.println("Home Page Handler");
+
+        // Add Dynamic value in form of Key & Values
+        model.addAttribute("name", "Samskara Soyabean Exporter");
+        model.addAttribute("YoutubeChannel", "Exporter of Soyabean from India");
+        model.addAttribute("GithubRepository", "https://github.com/patel812/scm2.0/commits/master/");
+
         return "home";
     }
 
